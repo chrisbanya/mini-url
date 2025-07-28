d68146b52b6ae985c4deb6da9825e2a7c9eb3d1b
Adabanya Christian <adabanyachristian21@gmail.com>
2025-07-29 00:51:24 +0100

    added localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BsCopy } from "react-icons/bs";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import UrlLists from "./components/UrlLists";
 
 function App() {
   const [formData, setFormData] = useState({
@@ -16,7 +17,16 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const apiKey = import.meta.env.VITE_TINYURL_API_KEY;
+  const [isVisible, setIsVisible] = useState(false);
+  const [shortenedLinks, setShortenedLinks] = useState(() => {
+    const saved = localStorage.getItem("shortenedLinks");
+    return saved ? JSON.parse(saved) : [];
+  });
   // console.log(apiKey)
+
+  useEffect(() => {
+    localStorage.setItem("shortenedLinks", JSON.stringify(shortenedLinks));
+  }, [shortenedLinks]);
 
   function handleChange(e) {
     setError("");
@@ -71,25 +81,24 @@ function App() {
       if (res.ok) {
         const data = await res.json();
         setResData(data.data);
-        setFormData((prev) => ({...prev, url: "", alias: "" }));
-        console.log("api response", resData);
-        setIsLoading(false);
+        setShortenedLinks((prev) => [data.data, ...prev]);
+        setFormData((prev) => ({ ...prev, url: "", alias: "" }));
+
+        // setIsLoading(false);
       } else {
-        // console.log("error:", res);
         const errorBody = await res.json();
-        console.error("Error details:", errorBody);
-        throw new Error(`Error: ${res.status} ${res.statusText}`);
+        const err = `: ${errorBody.errors[0]}`;
+        throw new Error(`Error: ${res.status} ${err}`);
       }
     } catch (error) {
       console.log(error);
       setError({ apiError: error.message });
-      // setIsLoading(false);
     } finally {
       setIsLoading(false);
     }
   }
 
-  async function handleCopyToClipboard({ tiny_url }) {
+  async function handleCopyToClipboard(tiny_url) {
     try {
       await navigator.clipboard.writeText(tiny_url);
       toast.success("Copied to clipboard !", {
@@ -102,10 +111,10 @@ function App() {
   }
 
   return (
-    <div className="w-11/12 mx-auto h-screen flex flex-col justify-center items-center">
-      <div className="border-2 flex justify-center items-center space-x-4">
-        <div>
-          <div className="text-2xl text-left font-semibold  border border-amber-500">
+    <div className="flex justify-center items-center min-h-screen mx-auto ">
+      <div className="w-11/12  flex ">
+        <div className="h-screen overflow-y-auto  flex flex-col md:w-1/2 pt-16 custom-scrollbar  ">
+          <div className="text-2xl text-left font-semibold   ">
             <h1 className="text-white">
               Effortlessly <span className="text-[#FF5E3A]">shorten</span> those
               long, pesky URLs with just a{" "}
@@ -115,7 +124,7 @@ function App() {
           </div>
           {/* default view */}
           {!Object.keys(resData).length > 0 && (
-            <div className="mt-4 p-5 rounded  bg-white">
+            <div className="mt-4 p-5 py-10  rounded  bg-white">
               <div className="mb-4">
                 <form
                   id="submitForm"
@@ -124,7 +133,7 @@ function App() {
                   onSubmit={handleSubmit}
                   className="flex flex-col space-y-4"
                 >
-                  <label>Shorten a long URL</label>
+                  <label className="font-semibold">Shorten a long URL</label>
                   <input
                     className={`border ${
                       error.url ? "border-red-500" : "border-gray-400"
@@ -138,7 +147,7 @@ function App() {
                   {error.url && (
                     <p className="text-red-500 text-sm mt-1">{error.url}</p>
                   )}
-                  <label>Customize your links</label>
+                  <label className="font-semibold">Customize your links</label>
                   <input
                     className={`border ${
                       error.alias ? "border-red-500" : "border-gray-400"
@@ -211,18 +220,35 @@ function App() {
                 </button>
               </div>
               <div className="flex justify-between">
-                <button className="px-4  py-2 w-[36%] border border-[#FF5E3A] text-[#FF5E3A] hover:bg-[#FF5E3A] hover:text-white font-semibold transition-colors duration-300 ease-in-out shadow rounded cursor-pointer">
+                <button
+                  type="button"
+                  disabled={isVisible === true}
+                  onClick={() => setIsVisible(!isVisible)}
+                  className="px-4  py-2 w-[36%] border border-[#FF5E3A] text-[#FF5E3A] hover:bg-[#FF5E3A] hover:text-white font-semibold transition-colors duration-300 ease-in-out shadow rounded cursor-pointer"
+                >
                   My URLs
                 </button>
-                <button onClick={() => setResData({})} className="px-4 py-2 w-[55%] bg-[#FF5E3A] font-bold text-white rounded">
+                <button
+                  onClick={() => setResData({})}
+                  className="px-4 py-2 w-[55%] bg-[#FF5E3A] font-bold text-white rounded"
+                >
                   Shorten another
                 </button>
               </div>
             </div>
           )}
           {/* end of conditional view */}
+
+          {/* Saved Url list  */}
+          {isVisible && (
+            <UrlLists
+              setIsVisible={setIsVisible}
+              shortenedLinks={shortenedLinks}
+            />
+          )}
         </div>
-        <div className="w-full border border-blue-600">
+
+        <div className="hidden md:block w-1/2 sticky top-0 h-screen ">
           <img
             src="/Happy student-storyset.png"
             alt="People illustration by storyset"
